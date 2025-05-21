220e7089d5f6a839000b287ae19c30c1bb39c7a2
Seshachalam Yerasala Venkata <seshachalam.yerasala.venkata@sap.com>
2025-05-21 16:43:30 +0530

    Refactor EtcdOperatorTask and EtcdOperatorTaskSpec comments for clarity and consistency

--- api/core/v1alpha1/etcdoperatortask.go
+++ api/core/v1alpha1/etcdoperatortask.go
@@ -56,17 +56,17 @@ type EtcdOperatorTask struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
+	// Spec defines the desired state of the EtcdOperatorTask resource.
 	// +kubebuilder:validation:Required
-	// +kubebuilder:validation:Immutable
-	// Spec is the specification of the EtcdOperatorTask resource.
 	Spec EtcdOperatorTaskSpec `json:"spec"`
-	// Status is most recently observed status of the EtcdOperatorTask resource.
+
+	// Status is the most recently observed status of the EtcdOperatorTask resource.
+	// +optional
 	Status EtcdOperatorTaskStatus `json:"status,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:object:root=true
-
 // EtcdOperatorTaskList contains a list of EtcdOperatorTask objects.
 type EtcdOperatorTaskList struct {
 	metav1.TypeMeta `json:",inline"`
@@ -75,34 +75,41 @@ type EtcdOperatorTaskList struct {
 }
 
 // EtcdOperatorTaskSpec defines the desired state of EtcdOperatorTask.
-// +kubebuilder:validation:Immutable
 type EtcdOperatorTaskSpec struct {
-	// +kubebuilder:validation:Immutable
-	// Config is task-specific key/value parameters. Only the relevant config for the task type should be set.
+
+	// Config defines the configuration for the EtcdOperatorTask.
+	// Only one of the configurations can be specified at a time.
 	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="The config field in spec is immutable and cannot be changed after creation."
 	Config EtcdOperatorTaskConfig `json:"config"`
 
-	// TTLSecondsAfterFinished is the time-to-live to garbage collect the
+	// TTLSecondsAfterFinished is the time-to-live (in seconds) to garbage collect the
 	// related resource(s) of the task once it has been completed.
 	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="The ttlSecondsAfterFinished field in spec is immutable and cannot be changed after creation."
 	// +optional
 	TTLSecondsAfterFinished *int32 `json:"ttlSecondsAfterFinished,omitempty"`
 
-	// OwnerEtcdReference refers to the name and namespace of the corresponding
+	// EtcdRef refers to the name and namespace of the corresponding
 	// Etcd owner for which the task has been invoked.
 	// +optional
-	EtcdRef *EtcdReference `json:"etcdRef"`
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="The etcdRef field in spec is immutable and cannot be changed after creation."
+	EtcdRef *EtcdReference `json:"etcdRef,omitempty"`
 }
 
+// EtcdOperatorTaskConfig defines the configuration for the EtcdOperatorTask.
+// Only one of the configurations can be specified at a time.
 type EtcdOperatorTaskConfig struct {
-	// Only set if Type is OnDemandSnapshot
-	OnDemandSnapshot *OnDemandSnapshotConfig `json:"onDemandSnapshotConfig,omitempty"`
+	// OnDemandSnapshotConfig specifies configuration for on-demand snapshot tasks.
+	// +optional
+	OnDemandSnapshot *OnDemandSnapshotConfig `json:"onDemandSnapshotConfig,omitempty"` // +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 }
 
 type EtcdReference struct {
 	// Name is the name of the Etcd resource.
 	// +kubebuilder:validation:Required
 	Name string `json:"name"`
+
 	// Namespace is the namespace of the Etcd resource.
 	// +kubebuilder:validation:Required
 	Namespace string `json:"namespace"`
@@ -111,18 +118,22 @@ type EtcdReference struct {
 type EtcdOperatorTaskStatus struct {
 	// State is the last known state of the task.
 	// +optional
-	State *TaskState `json:"state"`
+	State *TaskState `json:"state,omitempty"`
+
 	// LastTransitionTime is the last time the task transitioned from one state to another.
 	// +optional
 	LastTransitionTime *metav1.Time `json:"lastTransitionTime,omitempty"`
+
 	// InitiatedAt is the time at which the task has moved from "pending" state to the inProgress state.
 	// +optional
 	InitiatedAt *metav1.Time `json:"initiatedAt,omitempty"`
+
 	// LastErrors represents the errors when processing the task.
 	// +kubebuilder:validation:MaxItems=10
 	// +optional
-	LastErrors []EtcdOperatorTaskLastError `json:"lastErrors"`
-	// Captures the last operation status if task involves many stages.
+	LastErrors []EtcdOperatorTaskLastError `json:"lastErrors,omitempty"`
+
+	// LastOperation captures the last operation status if task involves many stages.
 	// +optional
 	LastOperation *EtcdOperatorLastOperation `json:"lastOperation,omitempty"`
 }
@@ -130,20 +141,26 @@ type EtcdOperatorTaskStatus struct {
 type EtcdOperatorTaskLastError struct {
 	// Code is an error code that uniquely identifies an error.
 	Code ErrorCode `json:"code"`
+
 	// Description is a human-readable message indicating details of the error.
 	Description string `json:"description"`
+
 	// ObservedAt is the time at which the error was observed.
 	ObservedAt metav1.Time `json:"observedAt"`
 }
 
 type EtcdOperatorLastOperation struct {
-	// Status of the last operation, one of pending, progress, completed, failed.
+	// State is the status of the last operation, one of pending, progress, completed, failed.
 	State OperationState `json:"state"`
-	// Phase represents the current phase of the operation wrt the interface methods
+
+	// Phase represents the current phase of the operation with respect to the interface methods.
 	Phase OperationPhase `json:"phase"`
+
 	// LastTransitionTime records the timestamp of the most recent state transition, marking when the operation moved from its previous state to the current value specified in the 'State' field.
-	LastTransitionTime *metav1.Time `json:"lastTransitionTime"`
-	// A human readable message indicating details about the last operation.
+	// +optional
+	LastTransitionTime *metav1.Time `json:"lastTransitionTime,omitempty"`
+
+	// Description is a human readable message indicating details about the last operation.
 	Description string `json:"description"`
 }
 
@@ -174,14 +191,16 @@ func (t *EtcdOperatorTask) HasTTLExpired() bool {
 }
 
 type OnDemandSnapshotConfig struct {
-	// Type of snapshot: "full" or "delta"
+	// Type specifies the type of snapshot: "full" or "delta".
 	// +kubebuilder:validation:Enum=full;delta
 	// +kubebuilder:validation:Required
-	Type OnDemandSnapshotType `json:"type,omitempty"`
+	Type OnDemandSnapshotType `json:"type"`
+
 	// IsFinal indicates if this is the final snapshot. Only applicable for full snapshots.
 	// +optional
-	IsFinal bool `json:"isFinal,omitempty"`
-	// Timeout in seconds for the snapshot operation
+	IsFinal *bool `json:"isFinal,omitempty"`
+
+	// TimeoutSeconds is the timeout in seconds for the snapshot operation.
 	// +optional
 	TimeoutSeconds *int32 `json:"timeoutSeconds,omitempty"`
 }
